1a51a151d903d17b65ed4e9ae586368bdd81ac90
Rama Al Ali <rama.muhammed.alali@gmail.com>
2025-12-10 11:08:39 +0000

    style:some fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["skz7lc-1337.csb.app"],
+  },
 });
